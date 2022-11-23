bac2e7ce1b1b33ce7722174d1a9b7f3cd08911dd
VanekAndreev2005 <118421152+VanekAndreev2005@users.noreply.github.com>
2022-11-23 16:04:30 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*оператор ==
+            string a = "hi";
+            string b = "hi";
+            Console.WriteLine(a == b);
+            Console.ReadLine();*/
+
+            /*оператор !=
+            string a = "Hi";
+            string b = "Hello";
+            Console.WriteLine(a != b);
+            Console.ReadLine();*/
+
+            /*метод Compare
+            string a = "B";
+            string b = "A";
+            int result = String.Compare(a, b);
+            Console.WriteLine(result);
+            Console.ReadLine();*/
+
+            /*метод Equals
+            string a = "Hello";
+            string b = "Hello";
+            if (object.ReferenceEquals(a, b))
+            {
+                Console.WriteLine("Объекты одинаковы");
+            }
+            else
+            {
+                Console.WriteLine("Разные объекты");
+            }
+            Console.ReadLine();*/
+
+            /*метод CompareOrdinal
+            string a = "A";
+            string b = "B";
+            Console.WriteLine(string.CompareOrdinal(a, b));
+            Console.ReadLine();*/
+
+            /*метод IndexOf
+            string s = "ivanprog";
+            int index;
+            Console.WriteLine(index = s.LastIndexOf("prog"));
+            Console.ReadLine();*/
+
+            /*метод Contains
+            String a = "Wakandaforever";
+            String b = "for";
+            String c = "For";
+            Console.WriteLine(a.Contains(b));
+            Console.WriteLine(a.Contains(c));
+            Console.ReadLine();*/
+        }
+    }
+}
